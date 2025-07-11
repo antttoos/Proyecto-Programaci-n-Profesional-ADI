154043c8061ef4c9d60a663cf8dbc9ed33353d5a
Daniel <danialvarez@alumnos.uai.cl>
2025-07-11 14:09:53 -0400

    searching bar

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/login(.*)", "/register"]);
+const isPublicRoute = createRouteMatcher(["/", "/login(.*)", "/register", "/api/scrape"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const url = new URL(req.url);
